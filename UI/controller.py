bb416695c7a037537a34efa1891a13a7a32eecb3
andreaGaudino <andrea.gaudino02@gmail.com>
2024-05-27 11:24:07 +0200

    creazione grafo

--- UI/controller.py
+++ UI/controller.py
@@ -16,3 +16,27 @@ class Controller:
 
     def handlePercorso(self, e):
         pass
+
+    def fillDDYear(self):
+        years = self._model.getYears()
+        yearsDD = list(map(lambda x: ft.dropdown.Option(x), years))
+        self._view._ddAnno.options = yearsDD
+        self._view.update_page()
+
+    def readDDTeams(self, e):
+        if e.control.data is None:
+            self.selectedTeam = None
+        else:
+            self.selectedTeam = e.control.data
+        #print(self.selectedTeam)
+    def handleYearDDSelection(self, e):
+        self.allTeams = self._model.getTeamsOfYear(self._view._ddAnno.value)
+        self._view._txtOutSquadre.clean()
+        self._view._txtOutSquadre.controls.append(ft.Text(f"Ho trovato {len(teams)} squadre che hanno giocato nell'anno {self._view._ddAnno.value}"))
+        for t in self.allTeams:
+            self._view._txtOutSquadre.controls.append(ft.Text(f"{t.teamCode}"))
+            self._view._ddSquadra.options.append(ft.dropdown.Option(data = t,
+                                                                     text = t.teamCode,
+                                                                     on_click=self.readDDTeams))
+        self._view.update_page()
+
--- model/model.py
+++ model/model.py
@@ -1,3 +1,41 @@
+import itertools
+
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self.allTeams = []
+        self.grafo = nx.Graph()
+
+    def buildGraph(self):
+        self.grafo.clear()
+        if len(self.allTeams) == 0:
+            print("Lista squadre vuota")
+            return
+        self.grafo.add_nodes_from(self.allTeams)
+
+
+        myedges = list(itertools.combinations(self.allTeams, 2))
+
+        self.grafo.add_edges_from(myedges)
+
+        #print(myedges)
+
+        # for t1 in self.grafo.nodes:
+        #     for t2 in self.grafo.nodes:
+        #         if t1 != t2:
+        #             self.grafo.add_edge(t1, t2)
+
+    def getYears(self):
+        return DAO.getAllYears()
+
+    def getTeamsOfYear(self, year):
+        self.allTeams = DAO.getTeamsOfYear(year)
+        return self.allTeams
+
+    def printGraphDetails(self):
+        print(f"Grafo creato con {len(self.grafo.nodes)} nodi e {len(self.grafo.edges)} archi")
+
